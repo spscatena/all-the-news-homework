bf5e62f662276cf5ba341265ebd02d1cddff32d1
Daniel Deverell <daniel.deverell@gmail.com>
2021-11-09 20:45:35 -0500

    done

--- src/components/App.js
+++ src/components/App.js
@@ -5,25 +5,47 @@ import Stories from "./Stories";
 
 const navItems = ["arts", "books", "fashion", "food", "movies", "travel"];
 const nytapi = "RuG9N6lD1Xss81PdRbmhuiJHjuiPEt6R";
-const section = "arts";
 
 function App() {
   const [stories, setStories] = React.useState([]);
   const [loading, setLoading] = React.useState(false);
+  const [section, setSection] = React.useState("arts");
 
   React.useEffect(() => {
+    const url = new URL(window.location.href);
+    const hash = url.hash.slice(1);
+    if (hash !== "undefined") {
+      console.log(" hash ", hash);
+      setSection(hash);
+    } else {
+      setSection("arts");
+    }
+  }, []);
+
+  React.useEffect(() => {
+    setLoading(true);
     fetch(
       `https://api.nytimes.com/svc/topstories/v2/${section}.json?api-key=${nytapi}`
     )
       .then((response) => response.json())
-      .then((data) => setStories(data.results));
-  }, []);
+      .then((data) => setStories(data.results))
+      .then(setLoading(false))
+      .catch((error) => {
+        console.log(error);
+      });
+  }, [section]);
 
   return (
     <>
       <Header siteTitle="All the news the Fits We Print" />
-      <Nav navItems={navItems} />
-      <Stories stories={stories} />
+
+      <Nav navItems={navItems} setSection={setSection} section={section} />
+
+      {loading || stories.length === 0 ? (
+        <h2>Loading...</h2>
+      ) : (
+        <Stories stories={stories} section={section} />
+      )}
     </>
   );
 }
---
+++ src/components/NavItem.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NavItem = (props) => {
+  const updateSection = (section) => {
+    props.setSection(section);
+  };
+
+  return (
+    <li>
+      <a
+        className={props.navItem === props.section ? "active" : ""}
+        href={`#${props.navItem}`}
+        onClick={() => updateSection(props.navItem)}
+      >
+        {props.navItem}
+      </a>
+    </li>
+  );
+};
+export default NavItem;
